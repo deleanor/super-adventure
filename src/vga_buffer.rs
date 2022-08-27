3eca3c5f91271e1eca409af6318ad4a94a682d61
Eleanor Dicharry <deleanor440@gmail.com>
2022-08-27 16:49:12 -0600

    Support formatted writes.

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use volatile::Volatile;
 
 #[allow(dead_code)]
@@ -90,12 +91,21 @@ impl Writer {
     }
 }
 
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
 pub fn print_something() {
+    use core::fmt::Write;
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe {&mut *(0xb8000 as *mut Buffer)},
     };
 
-    writer.write_string("Starting EleanorOS");
+    writer.write_string("Starting EleanorOS - ");
+    write!(writer, "Here is a number: {}", 1.0 / 3.0).unwrap();
 }
