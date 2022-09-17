2bcbdc091ff318a51e3dd87a1841d44c1ff3f659
Eleanor Dicharry <deleanor440@gmail.com>
2022-09-17 12:30:07 -0600

    Add an interrupt handler.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 #![no_std]
 #![no_main]
 
-mod vga_buffer;
+use rust_os::vga_buffer;
+use rust_os::interrupts;
+use rust_os::println;
 
 use core::panic::PanicInfo;
 
 static OS_NAME: &'static str = "SmallOS";
 
+pub fn init() {
+    interrupts::init_idt();
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
+
     vga_buffer::change_color(
-        vga_buffer::Color::Yellow, vga_buffer::Color::Blue);
+        vga_buffer::Color::Yellow, vga_buffer::Color::DarkGray);
     vga_buffer::clear_screen();    
     
     println!("Starting {}", OS_NAME);
     println!("Another line.");
+
+    x86_64::instructions::interrupts::int3();
+
+    println!("Made it here.");
 
     loop {}
 }
